20ff49f9ca9c002966eeef6d95283c3511c12d6f
dadi744 <dadibanjaransari74@gmail.com>
2022-10-28 11:13:16 +0700

    add responsive & script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-var word1 = "Welcome to"
-var word2 = "my portfolio,"
-var word3 = " my friend!"
-var full = word1 + word2 + word3;
-alert(full);
+alert("Welcome to my portfolio!");
+function tampilkan_highSchool() {
+    document.getElementById("hasil-highSchool").innerHTML = "<br><h3>SMA N 1 Ngawi</h3>" + "<p>2015 - 2018</p><br>";
+}
+function tampilkan_univ() {
+    document.getElementById("hasil-univ").innerHTML = "<br><h3>Brawijaya University</h3>" + "<p>2018 - 2022</p>";
+}
